836bf24c842baf6f4f28211da276907bffc47ced
Unknown <ing.damianfernandez@gmail.com>
2017-06-15 22:23:48 -0600

    Adding startup banner

---
+++ src/main/java/com/backbase/transactions/swagger/ApplicationLoadListener.java
@@ -0,0 +1,17 @@
+package com.backbase.transactions.swagger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class ApplicationLoadListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final String BANNER = "\r\n  _______                             _   _                 \r\n |__   __|                           | | (_)                \r\n    | |_ __ __ _ _ __  ___  __ _  ___| |_ _  ___  _ __  ___ \r\n    | | '__/ _` | '_ \\/ __|/ _` |/ __| __| |/ _ \\| '_ \\/ __|\r\n    | | | | (_| | | | \\__ \\ (_| | (__| |_| | (_) | | | \\__ \\\r\n    |_|_|  \\__,_|_| |_|___/\\__,_|\\___|\\__|_|\\___/|_| |_|___/";
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationLoadListener.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        LOGGER.info(BANNER);
+    }
+}
--- src/main/java/com/backbase/transactions/swagger/SwaggerConfiguration.java
+++ src/main/java/com/backbase/transactions/swagger/SwaggerConfiguration.java
@@ -4,5 +4,4 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 public class SwaggerConfiguration {
-
 }
